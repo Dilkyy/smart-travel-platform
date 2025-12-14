0e37bfb65e0164ab062d17a7ea7814a326529afe
E.L.D.Chamika <chamikadilki9@gmail.com>
2025-12-14 14:55:09 +0530

    Delete payment-service directory (Nested issue)

--- payment-service/payment-service/src/main/java/com/smarttravel/paymentservice/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.smarttravel.paymentservice.repository;
-
-import com.smarttravel.paymentservice.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
--- payment-service/payment-service/src/main/java/com/smarttravel/paymentservice/service/PaymentService.java
+++
@@ -1,9 +0,0 @@
-
-package com.smarttravel.paymentservice.service;
-
-import com.smarttravel.paymentservice.dto.PaymentRequestDTO;
-import com.smarttravel.paymentservice.dto.PaymentResponseDTO;
-
-public interface PaymentService {
-    PaymentResponseDTO processPayment(PaymentRequestDTO request);
-}
