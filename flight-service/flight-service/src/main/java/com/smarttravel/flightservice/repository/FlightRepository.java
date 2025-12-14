064c1bee44da0603a75f995a6ecf2734bddbafde
E.L.D.Chamika <chamikadilki9@gmail.com>
2025-12-14 14:54:19 +0530

    Delete flight-service directory (Coz its nested)

--- flight-service/flight-service/src/main/java/com/smarttravel/flightservice/repository/FlightRepository.java
+++
@@ -1,6 +0,0 @@
-package com.smarttravel.flightservice.repository;
-import com.smarttravel.flightservice.entity.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlightRepository extends JpaRepository<Flight, Long> {
-}
