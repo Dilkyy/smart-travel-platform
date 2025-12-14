8c06142e2a40af8a97d4e273a8b46399ffc22664
E.L.D.Chamika <chamikadilki9@gmail.com>
2025-12-14 14:54:39 +0530

    Delete hotel-service directory (Coz its nested)

--- hotel-service/hotel-service/src/main/java/com/smarttravel/hotelservice/repository/HotelRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.smarttravel.hotelservice.repository;
-
-import com.smarttravel.hotelservice.entity.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-}
--- hotel-service/hotel-service/src/main/java/com/smarttravel/hotelservice/service/HotelService.java
+++
@@ -1,8 +0,0 @@
-package com.smarttravel.hotelservice.service;
-
-import com.smarttravel.hotelservice.dto.HotelDTO;
-
-public interface HotelService {
-    HotelDTO getHotelById(Long id);
-    HotelDTO createHotel(HotelDTO dto);
-}
